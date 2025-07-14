cd2259353c1143164f6534678d9ca5340f777dae
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2025-07-14 15:21:45 +0600

    connection

---
+++ config.py
@@ -0,0 +1,4 @@
+host = "localhost"
+user = "your_user"
+password = "ypur_password"
+db_name = "ypur_db_name"
---
+++ main.py
@@ -0,0 +1,15 @@
+import pymysql
+from config import host, user, password, db_name
+try:
+    connection = pymysql.connect(
+    host=host,
+    port = 3306,
+    user=user,
+    database = db_name, 
+    cursorclass = pymysql.cursors.DictCursor
+)
+    print ("Successfully connected ...")
+except Exception as ex:
+    print("Connection refused...")
+    print(ex)
+
